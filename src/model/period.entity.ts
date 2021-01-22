63b9e74459e9930fb0fdf3b93e5e3219177f9e10
Edi Santoso <repodevs@gmail.com>
2021-01-22 16:04:00 +0700

    Added Period entity

---
+++ src/model/period.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, Column, Unique } from 'typeorm';
+import { PtcBaseEntity } from './base.entity';
+import { PeriodState } from './utils/enum';
+
+@Entity('period')
+@Unique('unique_year_month', ['year', 'month'])
+export class Period extends PtcBaseEntity {
+  @Column({ type: 'varchar', length: 250 })
+  name: string;
+
+  @Column({ type: 'date', name: 'start_date' })
+  startDate: Date;
+
+  @Column({ type: 'date', name: 'end_date' })
+  endDate: Date;
+
+  // This is period "number".
+  @Column({ type: 'smallint', name: 'month' })
+  month: Number;
+
+  @Column({ type: 'smallint', name: 'year' })
+  year: Number;
+
+  @Column({ type: 'date', name: 'close_date', nullable: true})
+  closeDate: Date;
+
+  @Column({ type: 'uuid', name: 'close_user_id', nullable: true})
+  closeUserId: string;
+
+  @Column({ type: 'enum', enum: PeriodState, default: PeriodState.OPEN })
+  state: PeriodState;
+}
