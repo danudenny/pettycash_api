4d1945dd6385418ee7871760824d548427b8df40
Edi Santoso <repodevs@gmail.com>
2021-02-15 11:01:09 +0700

    Added Seeder for Partner,AccountCoa,GlobalSetting

--- src/scripts/seeder.ts
+++ src/scripts/seeder.ts
@@ -1,9 +1,15 @@
 // tslint:disable: no-console
 import { ConnectionOptions, createConnection } from 'typeorm';
 import { LoaderEnv } from '../config/loader';
+import { AccountCoa } from '../model/account-coa.entity';
 import { Branch } from '../model/branch.entity';
+import { GlobalSetting } from '../model/global-setting.entity';
+import { Partner } from '../model/partner.entity';
 import { User } from '../model/user.entity';
+import AccountCoaSeed from '../seeders/account-coa.seed';
 import { BranchSeed } from '../seeders/branch.seed';
+import GlobalSettingSeed from '../seeders/global-setting.seed';
+import PartnerSeed from '../seeders/partner.seed';
 import { UserSeed } from '../seeders/user.seed';
 
 async function run() {
@@ -17,6 +23,15 @@ async function run() {
 
   // seed data user
   await connection.getRepository(User).insert(UserSeed);
+
+  // seed data partner
+  await connection.getRepository(Partner).save(PartnerSeed);
+
+  // seed data account coa
+  await connection.getRepository(AccountCoa).save(AccountCoaSeed);
+
+  // seed data global setting
+  await connection.getRepository(GlobalSetting).save(GlobalSettingSeed);
 
   // Close connection after running seeder.
   await connection.close();
