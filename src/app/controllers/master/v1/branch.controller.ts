2064699ef47af10b7c1e721a8ae31fe09b4cce2a
Edi Santoso <repodevs@gmail.com>
2021-02-23 14:37:40 +0700

    NPC-227: Update API GET /v1/branches

--- src/app/controllers/master/v1/branch.controller.ts
+++ src/app/controllers/master/v1/branch.controller.ts
@@ -1,14 +1,24 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { QueryBranchDTO } from '../../../domain/branch/branch.query.dto';
+import { BranchWithPaginationResponse } from '../../../domain/branch/response.dto';
 import { BranchService } from '../../../services/master/v1/branch.service';
 
-@ApiTags('Master Branch')
-@Controller('v1/master/branches')
+@ApiTags('Branch')
+@Controller('v1/branches')
 export class BranchController {
   constructor(private svc: BranchService) {}
 
   @Get()
-  public async list(@Query() query: any) {
+  @ApiOperation({ summary: 'List all Branch' })
+  @ApiOkResponse({ type: BranchWithPaginationResponse })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  public async list(@Query() query: QueryBranchDTO) {
     return await this.svc.list(query);
   }
 }
--- src/app/services/master/v1/branch.service.ts
+++ src/app/services/master/v1/branch.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { QueryBuilder } from 'typeorm-query-builder-wrapper';
 import { Branch } from '../../../../model/branch.entity';
+import { QueryBranchDTO } from '../../../domain/branch/branch.query.dto';
+import { BranchWithPaginationResponse } from '../../../domain/branch/response.dto';
 
 @Injectable()
 export class BranchService {
@@ -10,10 +13,30 @@ export class BranchService {
     private readonly repo: Repository<Branch>,
   ) {}
 
-  public async list(query: any): Promise<any> {
-    const data = await this.repo.find({ where: { isDeleted: false } });
+  public async list(query: QueryBranchDTO): Promise<any> {
+    const params = { limit: 10, ...query };
+    const qb = new QueryBuilder(Branch, 'b', params);
 
-    return { status: 'success', data };
+    qb.fieldResolverMap['name__contains'] = 'b.branch_name';
+    qb.fieldResolverMap['code__contains'] = 'b.branch_code';
+
+    qb.applyFilterPagination();
+    qb.selectRaw(
+      ['b.id', 'id'],
+      ['b.branch_name', 'name'],
+      ['b.branch_code', 'code'],
+    );
+    qb.andWhere(
+      (e) => e.isActive,
+      (v) => v.isTrue(),
+    );
+    qb.andWhere(
+      (e) => e.isDeleted,
+      (v) => v.isFalse(),
+    );
+
+    const branch = await qb.exec();
+    return new BranchWithPaginationResponse(branch, params);
   }
 
   public static async processQueueData(data: any) {
