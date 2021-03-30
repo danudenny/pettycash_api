3d7e2c6a079820680d5210e885f669666a7ef7de
Edi Santoso <repodevs@gmail.com>
2021-03-30 16:42:53 +0700

    NPC-535: Add API GET /v1/journals

--- src/app/domain/journal/response.dto.ts
+++ src/app/domain/journal/response.dto.ts
@@ -1,4 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Journal } from '../../../model/journal.entity';
 import { BasePaginationResponse } from '../common/base-pagination-response.dto';
 import { BaseResponse } from '../common/base-response.dto';
 import { PaginationBuilder } from '../common/pagination-builder';
@@ -6,16 +7,16 @@ import { JournalDTO } from './journal.dto';
 import { JournalResponseMapper } from './response.mapper';
 
 export class JournalWithPaginationResponse extends BaseResponse {
-  constructor(data?: Partial<JournalDTO | JournalDTO[]>, params?: any) {
+  constructor(data?: Journal[], params?: any) {
     super();
     if (data) {
-      this.data = JournalResponseMapper.fromDTO(data);
+      this.data = JournalResponseMapper.fromEntity(data);
       this.meta = PaginationBuilder.build(data, params);
     }
   }
 
   @ApiPropertyOptional({ type: () => [JournalDTO] })
-  data?: JournalDTO | JournalDTO[] = null;
+  data?: JournalDTO[] = null;
 
   @ApiPropertyOptional({ type: () => BasePaginationResponse })
   meta?: BasePaginationResponse;
