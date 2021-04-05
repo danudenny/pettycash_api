b9016a6695f3f5a29eca11f5bdefab65f5663abe
Edi Santoso <repodevs@gmail.com>
2021-04-05 15:21:49 +0700

    Update JournalState to add SS_SPV_HO

--- src/model/utils/enum.ts
+++ src/model/utils/enum.ts
@@ -121,8 +121,7 @@ export enum AccountTaxPartnerType {
 
 export enum JournalState {
   DRAFT = 'draft',
-  APPROVED_BY_SS_HO = 'approved_by_ss_ho',
-  APPROVED_BY_SPV_HO = 'approved_by_spv_ho',
+  APPROVED_BY_SS_SPV_HO = 'approved_by_ss_spv_ho',
   APPROVED_BY_TAX = 'approved_by_tax',
   POSTED = 'posted',
 }
