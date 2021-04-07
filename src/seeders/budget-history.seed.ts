9140fe10ff2b026bfbd0a51c962edc1a61c86532
Darmadi1604 <darmadi1604@github.com>
2021-04-07 10:48:01 +0700

    update

---
+++ src/seeders/budget-history.seed.ts
@@ -0,0 +1,26 @@
+export const BudgetHistorySeed = [
+  {
+    id: '5cceb2ea-0a2e-4167-bdb1-065c2c0be586',
+    createdAt: new Date(),
+    createUserId: '3aa3eac8-a62f-44c3-b53c-31372492f9a0',
+    updatedAt: new Date(),
+    updatedUserId: '3aa3eac8-a62f-44c3-b53c-31372492f9a0',
+    budgetId: '7d85ec0f-c10c-4078-a3cd-228f9db9447b',
+    endDate: '2021-01-23',
+    state: 'draft',
+    rejectedNote: null,
+    isDeleted: false,
+  },
+  {
+    id: 'b2ef8c65-e4b6-49dd-9aaf-a87246914616',
+    createdAt: new Date(),
+    createUserId: '3aa3eac8-a62f-44c3-b53c-31372492f9a0',
+    updatedAt: new Date(),
+    updatedUserId: '3aa3eac8-a62f-44c3-b53c-31372492f9a0',
+    budgetId: '97708a13-0a1d-48f0-9598-257961aa4118',
+    endDate: '2021-01-16',
+    state: 'draft',
+    rejectedNote: null,
+    isDeleted: false,
+  }
+];
