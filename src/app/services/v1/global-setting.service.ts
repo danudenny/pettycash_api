bc155ef8b4259fe47b68789afc57862591bf8088
Edi Santoso <repodevs@gmail.com>
2021-06-07 03:45:37 +0700

    Update API GlobalSetting to include partnerExpirationInMonth

--- src/app/services/v1/global-setting.service.ts
+++ src/app/services/v1/global-setting.service.ts
@@ -1,6 +1,6 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { GlobalSetting } from '../../../model/global-setting.entity';
-import { Repository } from 'typeorm';
+import { Repository, getConnection } from 'typeorm';
 import { GlobalSettingResponse } from '../../domain/global-setting/response.dto';
 import { UpdateGlobalSettingDTO } from '../../domain/global-setting/global-setting.payload.dto';
 
@@ -18,12 +18,17 @@ export class GlobalSettingService {
         'downPaymentPerdinCoa',
         'downPaymentReimbursementCoa',
       ],
+      cache: {
+        id: 'global_settings',
+        milliseconds: 1440 * 60000, // 1 day
+      },
     });
     return new GlobalSettingResponse(setting);
   }
 
   async update(payload: UpdateGlobalSettingDTO): Promise<any> {
     await this.settingRepo.update({}, payload);
+    await getConnection().queryResultCache?.remove(['global_settings']);
     return;
   }
 }
