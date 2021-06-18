266d8099c68d0de0ffb407e6d941da224f2fceb6
Denny Danuwijaya <danudenny@gmail.com>
2021-06-18 20:53:57 +0700

    fix endpoint report saldo

--- src/app/controllers/v1/allocation-balance.controller.ts
+++ src/app/controllers/v1/allocation-balance.controller.ts
@@ -9,7 +9,7 @@ import { TransferBalanceDTO } from '../../domain/balance/transfer-balance.dto';
 import { AllocationBalanceDetailResponse } from '../../domain/allocation-balance/dto/allocation-balance-detail.dto';
 import { RevisionAllocationBalanceDTO } from '../../domain/allocation-balance/dto/allocation-balance-revision.dto';
 
-@Controller('v1/allocation-balances')
+@Controller('v1/allocation-balance')
 @ApiTags('Cash Allocation Balance')
 export class AllocationBalanceController {
   constructor(private allocBallanceService: AllocationBalanceService) {
