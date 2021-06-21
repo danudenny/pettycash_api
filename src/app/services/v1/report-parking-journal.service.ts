751205cd18c2475b8e621d0965e17b9282b597d4
Edi Santoso <repodevs@gmail.com>
2021-06-22 03:04:38 +0700

    Init API Report Parking Journal

---
+++ src/app/services/v1/report-parking-journal.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ReportParkingJournalService {
+  public async list(query?: any) {
+    // TODO
+  }
+
+  public async export(query?: any) {
+    // TODO: add export flow.
+  }
+}
