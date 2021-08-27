a32422f8f747fa1f1730b194f9b3e062471cf551
Edi Santoso <repodevs@gmail.com>
2021-08-27 16:02:28 +0700

    Fix Entity CashBalanceAllocationOdoo

--- src/model/cash.balance.allocation-odoo.entity.ts
+++ src/model/cash.balance.allocation-odoo.entity.ts
@@ -3,54 +3,55 @@ import { PtcBaseEntity } from './base.entity';
 
 @Entity('cash_balance_allocation_odoo')
 export class CashBalanceAllocationOdoo extends PtcBaseEntity {
-	@Column({
-		name: 'auth_key',
-		default: '2ee2cec3302e26b8030b233d614c4f4e',
-		nullable: true
-	})
-	authKey: string;
+  @Column({
+    name: 'auth_key',
+    default: '2ee2cec3302e26b8030b233d614c4f4e',
+    nullable: true,
+  })
+  authKey: string;
 
-	@Column({
-		type: 'varchar',
-		length: 30
-	})
-	number: string;
+  @Column({
+    type: 'varchar',
+    length: 30,
+  })
+  number: string;
 
-	@Column({
-		name: 'analytic_account',
-		nullable: true,
-		default: '1201001',
-	})
-	analyticAccount: string;
+  @Column({
+    name: 'analytic_account',
+    nullable: true,
+    default: '1201001',
+  })
+  analyticAccount: string;
 
-	@Column({
-		name: 'branch_name'
-	})
-	branchName: string;
+  @Column({
+    name: 'branch_name',
+  })
+  branchName: string;
 
-	@Column({
-		type: 'numeric',
-		name: 'amount',
-		default: 0
-	})
-	amount: number;
+  @Column({
+    type: 'numeric',
+    name: 'amount',
+    default: 0,
+  })
+  amount: number;
 
-	@Column({
-		name: 'account_number'
-	})
-	accountNumber: string;
+  @Column({
+    name: 'account_number',
+    nullable: true,
+  })
+  accountNumber: string;
 
-	@Column({
-		type: 'text',
-		name: 'description',
-		nullable: true
-	})
-	description?: string;
+  @Column({
+    type: 'text',
+    name: 'description',
+    nullable: true,
+  })
+  description?: string;
 
-	@Column({
-		name: 'is_processed',
-		nullable: false,
-		default: () => 'false',
-	})
-	isProcessed: boolean;
+  @Column({
+    name: 'is_processed',
+    nullable: false,
+    default: () => 'false',
+  })
+  isProcessed: boolean;
 }
