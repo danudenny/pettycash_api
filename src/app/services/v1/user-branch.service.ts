c66f2d6aff9281b44e27814b3fd794ca780cedf5
Ery <ertantyo.edi@sicepat.com>
2021-09-29 07:59:58 +0700

    refactor: update to existing style

--- src/app/services/v1/user-branch.service.ts
+++ src/app/services/v1/user-branch.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { getManager } from 'typeorm';
 import { UserBranchResponse } from '../../domain/user-branch/response.dto';
+import { UserBranchDTO } from '../../domain/user-branch/user-branch.dto';
 
 @Injectable()
 export class UserBranchService {
   async get(xUsername: string): Promise<UserBranchResponse> {
-    let userBranch = await getManager().query(
-      `SELECT 
+    let userBranch = (
+      await getManager().query(
+        `SELECT 
         users.id AS userId, 
         first_name AS fistName, 
         last_name AS lastName,
         (SELECT ARRAY(SELECT branch_id FROM user_branch WHERE user_id = id)) AS branch_ids
       FROM users 
       WHERE username = '${xUsername}'`,
-    );
-    let response: UserBranchResponse = {
-      status: 'success',
-      data: userBranch,
-    };
-    return response;
+      )
+    )[0] as UserBranchDTO;
+    return new UserBranchResponse(userBranch);
   }
 }
