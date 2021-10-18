c7495f358bf252f65729a094eb5a635df4ecf39f
klittdj <danudenny@gmail.com>
2021-10-18 12:36:13 +0700

    fix employee filter & add API employee role

--- src/app/domain/employee/employee-response.dto.ts
+++ src/app/domain/employee/employee-response.dto.ts
@@ -5,6 +5,8 @@ import { EmployeeDTO } from './employee.dto';
 import { EmployeeResponseMapper } from './employee-response.mapper.dto';
 import { PaginationBuilder } from '../common/pagination-builder';
 import { BasePaginationResponse } from '../common/base-pagination-response.dto';
+import { EmployeeRoleDTO } from './employee-role.dto';
+import { EmployeeRoleResponseMapper } from './employee-role.response.mapper.dto';
 
 export class EmployeeResponse extends BaseResponse {
   constructor(data?: Partial<Employee | Employee[]>) {
@@ -29,6 +31,25 @@ export class EmployeeWithPaginationResponse extends BaseResponse {
 
   @ApiPropertyOptional({ type: () => [EmployeeDTO] })
   data?: EmployeeDTO | EmployeeDTO[] = null;
+
+  @ApiPropertyOptional({ type: () => BasePaginationResponse })
+  meta?: BasePaginationResponse;
+}
+
+export class EmployeeRoleWithPaginationResponse extends BaseResponse {
+  constructor(
+    data?: Partial<EmployeeRoleDTO | EmployeeRoleDTO[]>,
+    params?: any,
+  ) {
+    super();
+    if (data) {
+      this.data = EmployeeRoleResponseMapper.fromDTO(data);
+      this.meta = PaginationBuilder.build(data, params);
+    }
+  }
+
+  @ApiPropertyOptional({ type: () => [EmployeeRoleDTO] })
+  data?: EmployeeRoleDTO | EmployeeRoleDTO[] = null;
 
   @ApiPropertyOptional({ type: () => BasePaginationResponse })
   meta?: BasePaginationResponse;
